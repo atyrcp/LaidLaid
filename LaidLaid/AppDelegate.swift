c1da08b9520fee2d59e9f409a93fc5514b82edd5
atyrcp <atyrcp@gmail.com>
2019-05-29 16:27:06 +0800

    initial commit

--- LaidLaid/AppDelegate.swift
+++ LaidLaid/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let viewcontroller = UIViewController(nibName: nil, bundle: nil)
+        let navigationcontroller = UINavigationController(rootViewController: viewcontroller)
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationcontroller
+        
         // Override point for customization after application launch.
         return true
     }
